5ea7f95c5c85cd12e559601ef4933e2a8a8e0783
Mario <mario@nordcomputer.de>
2019-12-10 00:13:35 +0100

    test

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 # Your credentials for opskins and your telegram bot
 # How to get opskinskey: You need an account at opskins.com - there you can generate your key at Account->Advanced Options
-# How to get telegram bottoken: https://core.telegram.org/bots#6-botfather 
+# How to get telegram bottoken: https://core.telegram.org/bots#6-botfather #
 bottoken="TOKEN-FROM-YOUR-TELEGRAM-BOT"
 opskinskey='YOUR-OPSKINS-KEY'
